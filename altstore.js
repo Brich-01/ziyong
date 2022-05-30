e1d7dceaa213740273e4ee07b7895c2b85558483
Brich-01 <81300849+Brich-01@users.noreply.github.com>
2022-05-31 00:46:55 +0800

    Create altstore.js

---
+++ altstore.js
@@ -0,0 +1,31 @@
+let obj = JSON.parse($response.body);
+obj ={
+        "data": {
+            "attributes": {
+                "first_name": "Yu",
+                "full_name": "Yu yue"
+            },
+            "id": "977816133",
+            "relationships": {
+                "memberships": {
+                    "data": [{
+                        "id": "90c47077-73d9-40d2-8Dec-9bd600a07ced",
+                        "type": "tier"
+                    }]
+                }
+            },
+            "type": "member"
+        },
+        "included": [{
+            "attributes": {
+                "full_name": "Lang Khach",
+                "patron_status": "active_patron"
+            },
+            "id": "90c47077-73d9-40d2-8Dec-9bd600a07ced",
+            "type": "member"
+        }],
+        "links": {
+            "self":"https://www.patreon.com/api/oauth2/v2/user/30618536"
+        }
+    }
+$done({body: JSON.stringify(obj)});
